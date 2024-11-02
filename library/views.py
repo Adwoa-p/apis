acb485f2c4034346776746cb681e9720aea0eaa3
Adwoa-p <pokuaaa158@gmail.com>
2024-11-02 02:27:42 +0000

    added status code to get method and changed status code for update method to 200

--- library/views.py
+++ library/views.py
@@ -21,7 +21,7 @@ def book_list(request):
     if request.method == 'GET':
         books = Library.objects.all()
         serializer = Library_Serializer(books, many = True)
-        return Response(serializer.data)
+        return Response(serializer.data, status = status.HTTP_200_OK )
     if request.method == 'POST':
         serializer = Library_Serializer(data=request.data)
         if serializer.is_valid():
@@ -42,13 +42,13 @@ def book_details(request,id):
 
     if request.method == 'GET':
         serializer = Library_Serializer(book)
-        return Response(serializer.data)
+        return Response(serializer.data,  status = status.HTTP_200_OK )
     
     elif request.method == 'PUT':
         serializer = Library_Serializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
+            return Response(serializer.data, status = status.HTTP_200_OK)
         else: 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
