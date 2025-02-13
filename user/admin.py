6c969045e20f9c5759926d3ccc1dc616692deb58
Adwoa-p <pokuaaa158@gmail.com>
2025-02-13 02:34:48 +0000

    creted class to test admin permissions

--- user/admin.py
+++ user/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        # return super().has_add_permission(request)
+        return True
+
+    def has_change_permission(self, request, obj = ...):
+        # return super().has_change_permission(request, obj)
+        return True
+    
+
+admin.site.register(User, UserAdmin)
+
